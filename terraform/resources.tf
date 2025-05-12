22c1960eba7e31e7152e3786fc64e9b990cb318e
Artturi Kerminen <kerminenartturi@gmail.com>
2025-05-12 21:41:39 +0300

    Fix configuration in Terraform

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -2,17 +2,19 @@ resource "proxmox_lxc" "basic" {
   target_node  = "huber"
   hostname     = "lxc-basic-tf-test"
   ostemplate   = "local:vztmpl/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
-  password     = "BasicLXCContainer"
+  password     = "rootroot"
   unprivileged = true
+  start = true
+  onboot = true
 
   rootfs {
     storage = "local-lvm"
-    size    = "4G"
+    size    = "8G"
   }
 
   network {
     name   = "eth0"
     bridge = "vmbr0"
-    ip     = "dhcp"
+    ip     = "192.168.1.120/24"
   }
 }
